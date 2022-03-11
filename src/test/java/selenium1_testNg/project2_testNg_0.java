a5382b3b4d01cebbb5cc2201ef8b861a34b93dd5
Anamika <72334601+noviicee@users.noreply.github.com>
2022-03-11 17:33:59 +0530

    Update project2_testNg_0.java

--- src/test/java/selenium1_testNg/project2_testNg_0.java
+++ src/test/java/selenium1_testNg/project2_testNg_0.java
@@ -19,7 +19,7 @@ public class project2_testNg_0 {
 	// each test method
 	@BeforeMethod
 	public void init() {
-		System.setProperty("webdriver.chrome.driver", "./lib/chromedriver");
+		System.setProperty("webdriver.chrome.driver", ".\\lib\\new\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://elearningm1.upskills.in/");
 		driver.manage().window().maximize();
